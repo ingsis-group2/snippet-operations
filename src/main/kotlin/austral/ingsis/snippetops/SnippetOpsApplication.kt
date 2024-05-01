2ad62568d981e658ab7a2347ca9b8b0f096f03ac
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-05-01 15:08:38 -0300

    First commit

---
+++ src/main/kotlin/austral/ingsis/snippetops/SnippetOpsApplication.kt
@@ -0,0 +1,11 @@
+package austral.ingsis.snippetops
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetOpsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetOpsApplication>(*args)
+}
---
+++ src/test/kotlin/austral/ingsis/snippetops/SnippetOpsApplicationTests.kt
@@ -0,0 +1,13 @@
+package austral.ingsis.snippetops
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetOpsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
