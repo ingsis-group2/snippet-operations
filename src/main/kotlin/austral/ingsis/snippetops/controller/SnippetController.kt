6cd6d638e8538900a25d7f2b8ee9559092af0ecb
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-06-08 23:25:24 -0300

    testing

--- src/main/kotlin/austral/ingsis/snippetops/controller/SnippetController.kt
+++ src/main/kotlin/austral/ingsis/snippetops/controller/SnippetController.kt
@@ -4,7 +4,7 @@ import austral.ingsis.snippetops.dto.SnippetCreate
 import austral.ingsis.snippetops.dto.SnippetLocation
 import austral.ingsis.snippetops.service.SnippetService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.ResponseEntity
+import org.springframework.http.*
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.client.RestTemplate
 
 @Controller
 @RequestMapping("/snippet")
 class SnippetController(
     @Autowired val snippetService: SnippetService,
+    @Autowired val restTemplate: RestTemplate,
 ) {
     @GetMapping("/{id}")
     @ResponseBody
@@ -37,5 +39,12 @@ class SnippetController(
     @ResponseBody
     fun greet(): String {
         return "hello stranger"
+    }
+
+    @GetMapping("/greet/permissions")
+    fun greetPermissions(): ResponseEntity<String> {
+        val url = "http://snippet-permissions:8080/snippet/greetBack"
+        val response = restTemplate.exchange(url, HttpMethod.GET, null, String::class.java)
+        return ResponseEntity(response.body, response.statusCode)
     }
 }
