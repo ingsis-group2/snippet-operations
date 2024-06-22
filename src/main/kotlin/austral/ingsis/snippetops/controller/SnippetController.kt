70ec4aa1e5c49d19ed5c8fde89b7a5a478433749
tomasfreile <freiletomy@gmail.com>
2024-06-22 18:29:13 -0300

    format

--- src/main/kotlin/austral/ingsis/snippetops/controller/SnippetController.kt
+++ src/main/kotlin/austral/ingsis/snippetops/controller/SnippetController.kt
@@ -9,7 +9,13 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.client.RestTemplate
 
 @Controller
