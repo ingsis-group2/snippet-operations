b73b33bb5f13788828bab558a3b74589983dcb89
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-06-29 00:50:10 -0300

    refactor

--- src/main/kotlin/austral/ingsis/snippetops/dto/operations/OperationsTestDTO.kt
+++ src/main/kotlin/austral/ingsis/snippetops/dto/operations/OperationsTestDTO.kt
@@ -1,9 +1,9 @@
 package austral.ingsis.snippetops.dto.operations
 
-data class OperationsTestDTO (
+data class OperationsTestDTO(
     val snippetId: Long,
     val version: String,
     val inputs: List<String>,
     val envs: Map<String, Any>,
-    val output: String
+    val output: String,
 )
--- src/main/kotlin/austral/ingsis/snippetops/service/TestCaseService.kt
+++ src/main/kotlin/austral/ingsis/snippetops/service/TestCaseService.kt
@@ -2,11 +2,9 @@ package austral.ingsis.snippetops.service
 
 import austral.ingsis.snippetops.repository.BucketRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 
 @Service
-class TestCaseService (
-    @Autowired @Qualifier("testCaseBucketRepository") val bucketRepository: BucketRepository
-) {
-}
+class TestCaseService(
+    @Autowired val bucketRepository: BucketRepository,
+)
