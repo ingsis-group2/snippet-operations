f55d4790e529eaa606fd74e61678b5ebf76b0577
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-06-29 18:39:50 -0300

    add name field and refactor output

--- src/main/kotlin/austral/ingsis/snippetops/dto/operations/OperationsTestDTO.kt
+++ src/main/kotlin/austral/ingsis/snippetops/dto/operations/OperationsTestDTO.kt
@@ -2,17 +2,19 @@ package austral.ingsis.snippetops.dto.operations
 
 data class OperationsTestDTO(
     val id: String,
+    val name: String,
     val snippetId: Long,
     val version: String,
     val inputs: List<String>,
     val envs: Map<String, Any>,
-    val output: String,
+    val output: List<String>,
 )
 
 data class CreateTestCase(
+    val name: String,
     val snippetId: Long,
     val version: String,
     val inputs: List<String>,
     val envs: Map<String, Any>,
-    val output: String,
+    val output: List<String>,
 )
