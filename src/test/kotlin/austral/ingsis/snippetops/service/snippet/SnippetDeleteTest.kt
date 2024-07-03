968b09aafcfb778c4f3515be2e03dfb6b3cd9cd2
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-07-03 14:55:15 -0300

    lint

--- src/test/kotlin/austral/ingsis/snippetops/service/snippet/SnippetDeleteTest.kt
+++ src/test/kotlin/austral/ingsis/snippetops/service/snippet/SnippetDeleteTest.kt
@@ -15,7 +15,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 import java.time.LocalDateTime
-import java.util.*
+import java.util.Optional
 
 class SnippetDeleteTest {
 
