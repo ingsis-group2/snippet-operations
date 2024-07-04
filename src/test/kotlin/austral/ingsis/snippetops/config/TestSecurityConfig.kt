20e4e3fb6626bb8122e49627d6c69df258082094
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-07-04 02:14:12 -0300

    lint

--- src/test/kotlin/austral/ingsis/snippetops/config/TestSecurityConfig.kt
+++ src/test/kotlin/austral/ingsis/snippetops/config/TestSecurityConfig.kt
@@ -11,7 +11,6 @@ import org.springframework.security.web.SecurityFilterChain
 @EnableWebSecurity
 @Profile("test")
 class TestSecurityConfig {
-
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
